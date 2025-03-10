7c2147e50d923b4fef50d09b5a51a41cbdc1c717
Samarkin Danil <samarkin.danil@yandex.ru>
2025-03-10 19:47:52 +0300

    feat(Posts): реализация кеширования при ошибке получения постов

---
+++ src/lib/components/shared/Notification.svelte
@@ -0,0 +1,17 @@
+<div class="notification">
+	<slot />
+</div>
+
+<style>
+	.notification {
+		position: fixed;
+		right: 20px;
+		top: 20px;
+		background: white;
+		padding: 20px;
+		border-radius: 15px;
+		max-width: 400px;
+		background: #404040;
+		color: #ffffff;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	/**
-	 * Dependence
+	 * Dependencies
 	 */
 	import PostsApi from '$lib/api/methods/posts';
 	import { getReasonPhrase } from 'http-status-codes';
@@ -11,29 +11,68 @@
 	import Feed from '$lib/components/feed/Feed.svelte';
 	import Loader from '$lib/components/shared/Loader.svelte';
 	import ErrorComponent from '$lib/components/shared/Error.svelte';
+	import Notification from '$lib/components/shared/Notification.svelte';
+	import type { ServerResponse } from '$lib/api/types/response/utils';
+	import type { IPost } from '$lib/api/types/models/post';
+	import type { IPostsResponse } from '$lib/api/types/response/posts';
+
+	/**
+	 * Constants
+	 */
+	const LOCAL_STORAGE_KEY = 'cachedPosts';
+
+	/**
+	 * State
+	 */
+	let posts: IPost[] = [];
+	let isLoading: boolean = true;
+	let error: string | null = null;
+	let showCacheNotification: boolean = false;
 
 	/**
 	 * Load data
 	 * @description Метод для получения постов
 	 */
-	const loadData = async () => {
-		const response = await PostsApi.getAll();
+	const loadData = async (): Promise<void> => {
+		try {
+			// Пытаемся загрузить данные из сети
+			const response = await PostsApi.getAll();
 
-		if (!response.success) {
-			throw new Error(getReasonPhrase(response.data.code));
+			if (!response.success) {
+				throw new Error(getReasonPhrase(response.data.code));
+			}
+
+			// Сохраняем данные в локальное хранилище
+			const fetchedPosts = response.data;
+			localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(fetchedPosts));
+			posts = fetchedPosts;
+		} catch (e) {
+			//  При возникновении ошибки пытаемся загрузить данные из кеша
+			const cachedPosts: string | null = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+			if (cachedPosts) {
+				posts = JSON.parse(cachedPosts) as IPost[];
+				showCacheNotification = true;
+			} else {
+				error = 'Не удалось получить данные';
+			}
+		} finally {
+			isLoading = false;
 		}
-
-		return response.data;
 	};
 
+	loadData();
 </script>
 
-{#await loadData()}
+{#if isLoading}
 	<Loader />
-{:then posts}
-	<Feed {posts} />
-{:catch e}
+{:else if error}
 	<ErrorComponent>
-		{e}
+		{error}
 	</ErrorComponent>
-{/await}
+{:else}
+	{#if showCacheNotification}
+		<Notification>Данные загружены из кеша</Notification>
+	{/if}
+	<Feed {posts} />
+{/if}
